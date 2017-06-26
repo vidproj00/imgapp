8d11b2a0149cc2b7fa89b264bd0f01af16313b67
mac <mac@macs-MacBook-Pro.local>
2017-06-26 12:45:39 +0200

    first commit

---
+++ controllers/home.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   index: function(req, res) {
+//     res.send('The home: index controller');
+//   }
+// };
+module.exports = {
+  index: function(req, res) {
+    res.render('index');
+  }
+};
---
+++ server/configure.js
@@ -0,0 +1,37 @@
+var path = require('path'),
+    routes = require('./routes'),
+    exphbs = require('express-handlebars'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    cookieParser = require('cookie-parser'),
+    morgan = require('morgan'),
+    methodOverride = require('method-override'),
+    errorHandler = require('errorhandler'),
+    moment = require('moment');
+
+module.exports = function(app) {
+  app.use(morgan('dev'));
+  app.use(bodyParser({
+    uploadDir:path.join(__dirname, 'public/upload/temp')
+  }));
+  app.use(methodOverride());
+  app.use(cookieParser('some-secret-value-here'));
+  routes(app);
+  app.use('/public/', express.static(path.join(__dirname, '../public')));
+
+  if ('develpment' === app.get('env')) {
+    app.use(errorHandler);
+  }
+  app.engine('handlebars', exphbs.create({
+    defaultLayout: 'main',
+    layoutsDir: app.get('views') + '/layouts',
+    partialsDir: [app.get('views') + '/partials'],
+    helpers: {
+      timeago: function(timestamp) {
+        return moment(timestamp).startOf('minute').fromNow();
+      }
+    }
+  }).engine);
+  app.set('view engine', 'handlebars');
+  return app;
+}
